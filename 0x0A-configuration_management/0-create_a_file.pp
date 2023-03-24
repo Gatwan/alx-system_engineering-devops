cd48bb6ca8d34abd95cae5b464cc03bffc52397a
Gatwan <abigailgathigia@gmail.com>
2023-03-24 14:16:01 +0300

    Fix code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # make a file
 file { 'school':
     path  => '/tmp/school',
-    mode  => '0744',
+  content => 'I love Puppet',
     owner => 'www-data',
     group => 'www-data',
-  content => 'I love Puppet'
+    mode  => '0744'
 }
