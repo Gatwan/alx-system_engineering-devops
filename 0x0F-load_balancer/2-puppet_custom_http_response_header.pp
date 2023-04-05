08e6269af63ffd1185f55a41e8b901d13a1e8dd2
Gatwan <abigailgathigia@gmail.com>
2023-04-05 13:32:05 +0300

    Fix error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,24 @@
 #Add a custom HTTP header with puppet
 
 exec { 'update':
-  provider => shell,
+  provider => 'shell',
   command  => 'sudo apt -y update',
   before   => Exec['install Nginx'],
 }
 
 exec { 'install Nginx':
-  provider => shell,
+  provider => 'shell',
   command  => 'sudo apt -y install nginx',
   before   => Exec['add_header'],
 }
 
 exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "/listen 80 default_server;/ a     add_header X-Served-By \$HOST;" /etc/nginx/sites-available/default',
+  provider    => 'shell',
+  command     => 'sudo sed -i "/listen 80 default_server;/ a     add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
   before      => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
-  provider => shell,
+  provider => 'shell',
   command  => 'sudo service nginx restart',
 }
