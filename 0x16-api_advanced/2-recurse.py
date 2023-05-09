71470412ee2d1fac49a7734f86cf4171f9c4eb41
Gatwan <abigailgathigia@gmail.com>
2023-05-09 17:30:02 +0300

    [Add]: 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" recursive function that queries the Reddit API and returns a
+    list containing the titles of all hot articles for a given subreddit """
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], params={}, counter=0):
+    """ Get titles of all hot articles for a given subreddit """
+    if (params.get('after') is None and counter != 0):
+        return hot_list
+
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    response = get(url, allow_redirects=False, headers={'User-Agent': "myKey"},
+                   params=params)
+    if response.status_code == 404:
+        return None
+    params['after'] = response.json().get('data').get('after')
+    params['before'] = response.json().get('data').get('before')
+
+    for res in response.json().get("data").get('children'):
+        hot_list.append(res.get("data").get("title"))
+    counter += 1
+    return recurse(subreddit, hot_list, params, counter)
