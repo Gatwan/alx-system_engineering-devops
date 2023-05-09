4ae4f2a56119a0e039331570128f1b38864572d0
Gatwan <abigailgathigia@gmail.com>
2023-05-09 17:06:07 +0300

    [Fix]: Style error

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,8 @@ from requests import get
 def top_ten(subreddit):
     """ Get first 10 hot posts for a subreddit """
     url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
-    response = get(url, allow_redirects=False, headers={'User-Agent': "myKey"}, params={'limit': 10})
+    response = get(url, allow_redirects=False, headers={'User-Agent': "myKey"},
+                   params={'limit': 10})
     if response.status_code == 404:
         print(None)
         return
