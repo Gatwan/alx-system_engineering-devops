21929b1c4d9f46f12bd413d83b2cb3c78e4fd39b
Gatwan <abigailgathigia@gmail.com>
2023-05-10 13:05:20 +0300

    Fixing error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """ Script that gathers data from an API """
+from requests import get
+from sys import argv
+
+
+def main():
+    total_tasks = 0
+    completed_tasks = 0
+    task_res = get("https://jsonplaceholder.typicode.com/todos")
+    user_res = get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    employee_name = user_res.json().get("name")
+
+    for task in task_res.json():
+        if task.get("userId") == int(argv[1]):
+            if task.get("completed"):
+                completed_tasks += 1
+            total_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, completed_tasks, total_tasks))
+
+    for task in task_res.json():
+        if task.get("userId") == int(argv[1]):
+            if task.get("completed"):
+                print("\t {}".format(task.get("title")))
+
+
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
-
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
-
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+    main()
